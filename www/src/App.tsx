cd61f4538e688358d7d6a42fb30254c3c11d2d1f
markwing6011 <mark@tunl.to>
2024-10-20 01:06:45 +0200

    Added from To react component using typescript

---
+++ www/src/App.tsx
@@ -0,0 +1,11 @@
+import Main from "./pages/sso/Main";
+
+function App() {
+  return (
+    <div className="App" style={{color: 'purple'}}>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
